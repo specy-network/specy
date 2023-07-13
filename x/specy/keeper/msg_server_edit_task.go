aca781a9e282819386f63e3a3f511cb932664957
shaojie <1654574171@qq.com>
2023-07-13 00:11:25 -0700

    Improve the processing logic of task management

--- x/specy/keeper/msg_server_edit_task.go
+++ x/specy/keeper/msg_server_edit_task.go
@@ -2,16 +2,46 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/specy-network/specy/x/specy/types"
+	"github.com/tendermint/tendermint/crypto/tmhash"
 )
 
 func (k msgServer) EditTask(goCtx context.Context, msg *types.MsgEditTask) (*types.MsgEditTaskResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	_, found := k.GetTask(ctx, msg.Creator, msg.Name)
+	if !found {
+		return nil, types.ErrTaskNotExsit
+	}
+	task := &types.Task{
+		Owner:        msg.Creator,
+		Name:         msg.Name,
+		Hash:         fmt.Sprintf("%x", tmhash.Sum([]byte(msg.Creator+msg.Name))),
+		ConnectionId: msg.ConnectionId,
+		Msg:          msg.Msg,
+		RuleFiles:    msg.RuleFiles,
+		TaskType:     msg.TaskType,
+		ScheduleType: &types.Condition{IntervalType: msg.IntervalType, Number: msg.Number},
+	}
+	k.SetTask(ctx, *task)
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			types.EventTypeEditTask,
+			sdk.NewAttribute(types.AttributeKeyCreator, msg.Creator),
+			sdk.NewAttribute(types.AttributeKeyTaskName, msg.Name),
+			sdk.NewAttribute(types.AttributeKeyTaskHash, task.Hash),
+			sdk.NewAttribute(types.AttributeKeyConnectId, msg.ConnectionId),
+			sdk.NewAttribute(types.AttributeKeyTaskMsgs, task.Msg.String()),
+			sdk.NewAttribute(types.AttributeKeyTaskRuleFile, task.RuleFiles),
+			sdk.NewAttribute(types.AttributeKeyTaskType, string(rune(task.TaskType))),
+			sdk.NewAttribute(types.AttributeKeyTaskIntervalType, string(rune(task.ScheduleType.IntervalType))),
+			sdk.NewAttribute(types.AttributeKeyTaskIntervalNumber, string(rune(task.ScheduleType.Number))),
+		),
+	})
 
 	return &types.MsgEditTaskResponse{}, nil
 }
--- x/specy/types/errors.go
+++ x/specy/types/errors.go
@@ -8,10 +8,10 @@ import (
 
 // x/specy module sentinel errors
 var (
-	ErrSample          = sdkerrors.Register(ModuleName, 1100, "sample error")
-	ErrExecutorIsExsit = sdkerrors.Register(ModuleName, 1, "create executor error , already exsit")
-	ErrTaskIsExsit     = sdkerrors.Register(ModuleName, 2, "create task error , already exsit")
-	ErrTaskPortParse   = sdkerrors.Register(ModuleName, 3, "task port parse error , error")
-
-	ErrTaskInterMsgsUnmarshal = sdkerrors.Register(ModuleName, 4, "task inter msgs unmarshal error , error")
+	ErrSample                 = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrExecutorIsExsit        = sdkerrors.Register(ModuleName, 1001, "create executor error , already exsit")
+	ErrTaskIsExsit            = sdkerrors.Register(ModuleName, 1002, "create task error , already exsit")
+	ErrTaskPortParse          = sdkerrors.Register(ModuleName, 1003, "task port parse error , error")
+	ErrTaskInterMsgsUnmarshal = sdkerrors.Register(ModuleName, 1004, "task inter msgs unmarshal error , error")
+	ErrTaskNotExsit           = sdkerrors.Register(ModuleName, 1005, "process task error,not exsit")
 )
